16b30826ed214659c147655d47003dd91d534d20
Lavorary <hei.lavorary@gmail.com>
2026-06-02 21:43:15 +0300

    feat/rework App.tsx, db and EmployeesList.tsx

--- src/employees/EmployeesList.tsx
+++ src/employees/EmployeesList.tsx
@@ -1,14 +1,10 @@
 import {
   List,
-  Datagrid,
-  TextField,
-  NumberField,
   BooleanField,
   SearchInput,
   SelectInput,
-  Pagination,
-  EditButton,
-  DeleteButton,
+  DataTable,
+  EmailField,
 } from "react-admin";
 
 const choices = [
@@ -29,24 +25,21 @@ const employeeFilters = [
 ];
 
 export const EmployeeList = () => (
-  <List
-    filters={employeeFilters}
-    perPage={2}
-    pagination={<Pagination rowsPerPageOptions={[2]} />}
-  >
-    <Datagrid rowClick="show">
-      <TextField source="firstname" label="First name" />
-      <TextField source="lastname" label="Last name" />
-      <TextField source="email" label="Email" />
-      <TextField source="department" label="Department" />
-      <NumberField
-        source="salary"
-        label="Salary"
-        options={{ style: "currency", currency: "EUR" }}
-      />
-      <BooleanField source="active" label="Actif" />
-      <EditButton />
-      <DeleteButton />
-    </Datagrid>
-  </List>
+  <List filters={employeeFilters}>
+
+
+        <DataTable>
+            <DataTable.Col source="id" />
+            <DataTable.Col source="firstname" />
+            <DataTable.Col source="lastname" />
+            <DataTable.Col source="email">
+                <EmailField source="email" />
+            </DataTable.Col>
+            <DataTable.Col source="department" />
+            <DataTable.NumberCol source="salary" />
+            <DataTable.Col source="online">
+                <BooleanField source="online" />
+            </DataTable.Col>
+        </DataTable>
+    </List>
 );
